071b5994373e8106bdb3d03bc067dca4ceba766e
Tobi Ebenezer <tobisamuel70@gmail.com>
2026-06-13 14:26:42 +0100

    chore:  initialize gitignore files, and refresh API documentation

--- social/cmd/api/main.go
+++ social/cmd/api/main.go
@@ -28,7 +28,7 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr: env.GetString("DB_ADDR", "postgres://postgres:Awodumila@localhost:5432/gosocial?sslmode=disable"),
+			addr: env.GetString("DB_ADDR", "postgres://postgres:password@localhost:5432/gosocial?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 25),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 25),
 			maxIdleTime: env.GetString("DB_MAX_IDLE_TIME", "15m"),
